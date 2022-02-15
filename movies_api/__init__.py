fe7da449df344b8c05c2f2f370e9b64b3a899fd6
Davi Castro <davialmeida@47jd@gmail.com>
2022-02-15 00:18:37 -0300

    feat: added instance file path and /movies to consume api

--- movies_api/__init__.py
+++ movies_api/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
     app.config['SECRET_KEY'] = 'jlkjjlkjsalkdjlks'
+    app.config.from_pyfile('config.py')
 
     from .views import views
 
--- movies_api/views.py
+++ movies_api/views.py
@@ -1,7 +1,14 @@
-from flask import Blueprint, render_template
+import requests
+from flask import Blueprint, render_template, request, current_app
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
     return render_template('home.html')
+
+@views.route('/movies')
+def get_movies():
+    url = f'https://api.themoviedb.org/3/movie/550?api_key={current_app.config["API_KEY"]}'
+    req = requests.get(url)
+    return req.json()
